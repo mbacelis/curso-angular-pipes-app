6784d567606bfb04a8f2b3ab74d69bcf985104fc
Mauricio Bacelis <mbacelis_@hotmail.com>
2022-07-13 18:45:13 -0500

    Se crea modulo centralizado para exportar concentrar y exportar los modulos de Prime Ng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ButtonModule } from 'primeng/button';
-import {CardModule} from 'primeng/card';
+
 
 import { AppComponent } from './app.component';
+import { PrimeNGModule } from './prime-ng/prime-ng.module';
 
 @NgModule({
   declarations: [
@@ -11,8 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    ButtonModule,
-    CardModule
+    PrimeNGModule
   ],
   providers: [],
   bootstrap: [AppComponent]

46f7b1e64f3e53b2be839725919320e295ed1099
Mauricio Bacelis <mbacelis_@hotmail.com>
2022-07-14 12:38:45 -0500

    Se agregan rutas de la aplicación y modulos de pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRouterModule } from './app-router.module';
 
 
 import { AppComponent } from './app.component';
@@ -11,6 +12,7 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
+    AppRouterModule,
     SharedModule
   ],
   providers: [],
